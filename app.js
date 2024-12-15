6107278cb8b3573b02c187bb7974a3747a34ff30
kb_germ <k.b.germ1806@gmail.com>
2024-12-15 21:32:21 +0300

    feat: gh-pages branch

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ var createNewTaskElement = function (taskString) {
 
 
 var addTask = function () {
-    console.log("Add Task...");
+    console.log("Add Task....");
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem = createNewTaskElement(taskInput.value);
